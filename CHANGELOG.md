265b42987d053244dbd2e8fc2663b6159854f193
Daniel Richmond <daniel.g.richmond@gmail.com>
2026-08-02 11:03:51 -0500

    docs: prepare comments, next adrs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 2026-08-01
 
 Initial commit. Week was spent learning PHP and MySQL while going over the initial data in the Sweetwater Test. Added ADR from 07-30.
+
+## 2026-08-02
+
+Added ADRs I will be researching for today, along with a comment outline for the first task. I suspect the ADR on how to read in the MySQL effectively will impact how I implement the second task in the script. Then again, Task 2 serves a different intent, and could be an entirely different script to perform its duties.
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Serve this using PHP's server so that it runs simply by opening the connection in a browser.
+
+// Start a connection to the MySQL database, prompting for credentials.
+
+// Query the database for the rows in the sweetwater table.
+// Following the readme, the table should be known.
+
+// Display an <h1> header with the text "Sweetwater Comments".
+
+echo "<h1>Sweetwater Comments</h1>";
+
+// Generate an <h2> header for each comment, displaying the comment's category.
+// Categories are "comments about candy", comments about call me / don't call me, comments about who referred me, comments about signature requirements on delivery, and miscellaneous comments (comments that don't match other categories).
+
