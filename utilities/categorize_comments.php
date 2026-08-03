f9b47ca7327f939c17f7289c9ddce60d815ed562
Daniel Richmond <daniel.g.richmond@gmail.com>
2026-08-02 20:10:39 -0500

    chore: formatting fixes

--- utilities/categorize_comments.php
+++ utilities/categorize_comments.php
@@ -8,9 +8,9 @@ function categorizeComments(
     array &$categorizedComments,
 ): void {
     $candy_regex = '/\\b(candy|chocolate|lollipop|taffy|sweets|bit o honey)\\b/i';
-$call_regex = '/\\b(call me|dont call|do not call|text me|phone|mobile|ring)\\b/i';
-$referral_regex = '/\\b(internet search|heard about|referred|googled|referral|recommended by|friend told me|sent me)\\b/i';
-$signature_regex = '/\\b(signature|sign for|adult signature)\\b/i';
+    $call_regex = '/\\b(call me|dont call|do not call|text me|phone|mobile|ring)\\b/i';
+    $referral_regex = '/\\b(internet search|heard about|referred|googled|referral|recommended by|friend told me|sent me)\\b/i';
+    $signature_regex = '/\\b(signature|sign for|adult signature)\\b/i';
     $matchedAnyCategory = false;
 
     if (preg_match($candy_regex, $normalized) === 1) {
