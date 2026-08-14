535f11aea871df146635f4027ce3cd6387e90036
Daniel Richmond <daniel.g.richmond@gmail.com>
2026-08-14 15:02:33 -0500

    feat: collapsible comments sections

--- utilities/render_comments.php
+++ utilities/render_comments.php
@@ -1,16 +1,20 @@
 <?php
 
 function renderComments(array $categorizedComments): void {
-	foreach ($categorizedComments as $heading => $comments) {
-		echo '<h2>' . htmlspecialchars($heading, ENT_QUOTES, 'UTF-8') . '</h2>';
+    foreach ($categorizedComments as $heading => $comments) {
+        echo '<details>';
+        echo '<summary>' . htmlspecialchars($heading, ENT_QUOTES, 'UTF-8') . '</summary>';
 
-		if (empty($comments)) {
-			echo '<p><em>No comments in this category.</em></p>';
-			continue;
-		}
+        if (empty($comments)) {
+            echo '<p><em>No comments in this category.</em></p>';
+            echo '</details>';
+            continue;
+        }
 
-		foreach ($comments as $commentHtml) {
-			echo '<p>' . $commentHtml . '</p>';
-		}
-	}
+        foreach ($comments as $commentHtml) {
+            echo '<p>' . $commentHtml . '</p>';
+        }
+
+        echo '</details>';
+    }
 }
